2739733ce3c403a34338d362c119740abef50890
Khoi Nguyen Minh <intern.nmkhoi@tma.com.vn>
2019-03-13 14:39:43 +0700

    reformate code

--- src/main/java/com/khoi/price/dao/IPriceDAO.java
+++ src/main/java/com/khoi/price/dao/IPriceDAO.java
@@ -6,13 +6,13 @@ import java.util.List;
 
 public interface IPriceDAO extends IBaseDAO<Price, Integer> {
 
-  public List<Integer> findPrices(int product_id);
+  List<Integer> findPrices(int product_id);
 
   //public List<Integer> findPrice(int product_id);
 
-  public List<Price> findProductPriceHistory(int product_id);
+  List<Price> findProductPriceHistory(int product_id);
 
-  public int findPrice2(int product_id);
+  int findPrice2(int product_id);
 
-  public Boolean deleteByProductId(int product_id);
+  Boolean deleteByProductId(int product_id);
 }
