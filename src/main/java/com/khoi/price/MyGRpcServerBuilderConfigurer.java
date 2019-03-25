0dca01ebb21efe4b8fec5e847864e3acf866dcbc
iohnkhnim <hiepsi1211@gmail.com>
2019-03-25 23:41:50 +0700

    add ssl authentication

--- src/main/java/com/khoi/price/MyGRpcServerBuilderConfigurer.java
+++ src/main/java/com/khoi/price/MyGRpcServerBuilderConfigurer.java
@@ -5,19 +5,20 @@ import io.grpc.netty.GrpcSslContexts;
 import io.grpc.netty.NettyServerBuilder;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.SslProvider;
-import java.io.File;
 import org.lognet.springboot.grpc.GRpcServerBuilderConfigurer;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
 
 @Component
 public class MyGRpcServerBuilderConfigurer extends GRpcServerBuilderConfigurer {
 
-  private final String certChainFilePath = "D:\\price\\src\\main\\java\\com\\khoi\\price\\key\\server.crt";
-  private final String privateKeyFilePath = "D:\\price\\src\\main\\java\\com\\khoi\\price\\key\\server.pem";
+  private final String certChainFilePath = "src/main/java/com/khoi/price/key/server.crt";
+  private final String privateKeyFilePath = "src/main/java/com/khoi/price/key/server.pem";
 
-  private SslContextBuilder getSslContextBuilder() throws Exception{
-    SslContextBuilder sslContextBuilder = SslContextBuilder
-        .forServer(new File(certChainFilePath), new File(privateKeyFilePath));
+  private SslContextBuilder getSslContextBuilder() throws Exception {
+    SslContextBuilder sslContextBuilder =
+        SslContextBuilder.forServer(new File(certChainFilePath), new File(privateKeyFilePath));
     return GrpcSslContexts.configure(sslContextBuilder, SslProvider.OPENSSL);
   }
 
